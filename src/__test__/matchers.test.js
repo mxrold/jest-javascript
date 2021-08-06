6f4801ee995bb2399b48cc8333328b9a84ac3452
Martin <martinroldan889@gmail.com>
2021-08-05 21:14:13 -0300

    Common matchers

---
+++ src/__test__/matchers.test.js
@@ -0,0 +1,27 @@
+describe('Comparadores comunes', () => {
+    const cities = {
+        argentina: 'Rosario', 
+        brasil: 'San Pablo',
+        colombia: 'Bogota',
+        chile: 'Santiago'
+    }
+
+    const cities2 = {
+        argentina: 'Rosario', 
+        brasil: 'San Pablo',
+        colombia: 'Bogota',
+        chile: 'Santiago'
+    }
+    
+    test('Verificando un elemento del objeto', () => {
+        expect(cities.argentina).toEqual('Rosario')
+    })
+
+    test('Comparando objetos', () => {
+        expect(cities).toEqual(cities2)
+    })
+
+    test('Los objetos no son iguales', () => {
+        expect(cities).not.toEqual(cities2)
+    })
+})
